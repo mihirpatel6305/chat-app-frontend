b2f26bb48986b8333a2ccab7cdb951d9506a2d2e
Mihir patel <mmcpatel6305@gmail.com>
2025-09-29 14:40:23 +0530

    fix: socket service

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_BASE_URL;
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 const socket = io(SOCKET_URL, {
   transports: ["websocket"],
