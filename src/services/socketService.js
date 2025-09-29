1f76739688e8bf5b4939090160097b1177e9929d
Mihir patel <mmcpatel6305@gmail.com>
2025-09-29 15:13:29 +0530

    fix:cors issue for websocket

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -3,6 +3,7 @@ import { io } from "socket.io-client";
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 const socket = io(SOCKET_URL, {
+  transports: ["websocket"],
   withCredentials: true, // important if you later use cookies/auth
 });
 
