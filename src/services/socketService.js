5e23f27ff577938de40bdf8d1887f0c019954bc8
Mihir patel <mmcpatel6305@gmail.com>
2025-09-29 14:45:10 +0530

    fix websocket thing

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -3,7 +3,6 @@ import { io } from "socket.io-client";
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 const socket = io(SOCKET_URL, {
-  transports: ["websocket"],
   withCredentials: true, // important if you later use cookies/auth
 });
 
