d140dcfdb6959b57c3c0a8d0bb86a588570fb68a
Mihir patel <mmcpatel6305@gmail.com>
2025-09-29 14:17:51 +0530

    test commit for host

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
-import Login from "../pages/login";
+import Login from "../pages/Login";
 import SignUp from "../pages/Signup";
 import ProtectedRoutes from "./ProtectedRoutes";
 import PublicRoutes from "./PublicRoutes";
